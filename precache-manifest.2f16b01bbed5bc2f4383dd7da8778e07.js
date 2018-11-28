92565a9784dd04f708b370bfefd51d088d7dacf4
ceyhuncelik <ceyhuncelik.kbu@gmail.com>
2018-11-28 21:04:05 +0300

    Updates

---
+++ precache-manifest.2f16b01bbed5bc2f4383dd7da8778e07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/enuygun/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0b14ad4854e5faa1da51",
+    "url": "/enuygun/static/js/runtime~main.0b14ad48.js"
+  },
+  {
+    "revision": "fd934ea4f3ffff923159",
+    "url": "/enuygun/static/js/main.fd934ea4.chunk.js"
+  },
+  {
+    "revision": "1f22f873945d81de7c5b",
+    "url": "/enuygun/static/js/1.1f22f873.chunk.js"
+  },
+  {
+    "revision": "fd934ea4f3ffff923159",
+    "url": "/enuygun/static/css/main.eeec9aff.chunk.css"
+  },
+  {
+    "revision": "f7921b5fabbb97ec5efbe50c624bb59f",
+    "url": "/enuygun/index.html"
+  }
+];
